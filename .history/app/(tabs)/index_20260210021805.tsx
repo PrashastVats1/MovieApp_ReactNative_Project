40228d14e4fbd4528239e43fd109ce212b64cd16
PrashastVats1 <prashastvats@gmail.com>
2026-02-10 02:24:31 +0530

    boiler plate code

--- .history/app/(tabs)/index_20260210021805.tsx
+++ .history/app/(tabs)/index_20260210021805.tsx
@@ -1,6 +1,7 @@
 import { Image } from "expo-image";
 import { Platform, StyleSheet } from "react-native";
 
+import { HelloWave } from "@/components/hello-wave";
 import ParallaxScrollView from "@/components/parallax-scroll-view";
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
@@ -19,6 +20,7 @@ export default function HomeScreen() {
     >
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Welcome to the Jungle!</ThemedText>
+        <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
