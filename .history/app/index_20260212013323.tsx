eec275528669c93c141b992d530d2af36838b452
PrashastVats1 <prashastvats@gmail.com>
2026-02-12 03:11:54 +0530

    added bottom panel for home, search, profile n saved

---
+++ .history/app/index_20260212013323.tsx
@@ -0,0 +1,16 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-dark-200 font-bold">Welcome</Text>
+      <Link href="/onboarding" className="mt-4 text-lg text-blue-500">
+        Onboarding
+      </Link>
+      <Link href="/movie/avengers" className="mt-4 text-lg text-blue-500">
+        Avengers
+      </Link>
+    </View>
+  );
+}
